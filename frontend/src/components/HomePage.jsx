801bf97b872931cfcde9bd1b13b5a022bce22f3a
Kapil <kapilishwarkar2004@gmail.com>
2026-02-13 06:23:01 +0530

    fix: fixed small bug

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Sidebar from './Sidebar'
 import MessageContainer from './MessageContainer'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+
+const { authUser } = useSelector((store) => store.user); // 👈 authUser not user
+const navigate = useNavigate();
+
+useEffect(() => {
+  if(!authUser){
+    navigate('/login');
+  }
+}, [authUser]); // 👈 add authUser as dependency
+
   return (
     <div className=" flex sm:h-[450px] md:h-[600px] bg-gray-0 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100 p-6 shadow-md" >
       <Sidebar/>
